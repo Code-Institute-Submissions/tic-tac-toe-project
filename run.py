99bf0ca74812eb97f1a2ce196ea3a5fbfc1f9464
Nelliesohl <nelliesohl@icloud.com>
2022-07-24 05:43:32 +0000

    Add basic code structure for project

--- run.py
+++ run.py
@@ -1,3 +1,35 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# --- Import libraries ---
+
+
+# --- Global variables ---
+
+
+# --- Game functions ---
+
+def print_game_board():
+# Will print the game board to the terminal
+
+def get_user_move():
+# Will get and validate user input 
+
+def get_computer_move():
+# Will generate and validate random computer move 
+
+def check_board_for_win():
+# Will check board for win
+
+def check_board_for_tie():
+# Will check board for tie
+
+def alternate_player():
+# Will alternate between the two players 
+
+def main():
+# Run all program functions
+
+def play_again()
+# Restarts game with the right user input
+pass
+
+# --- Start execution --- 
+main()
